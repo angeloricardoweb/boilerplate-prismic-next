355bace0cf5ba77289ec6259ade411878112c7e1
Angelo Ricardo <angeloengcomp@gmail.com>
2022-10-05 01:16:56 -0300

    done(test): testando pwa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex items-center justify-center flex-col gap-5 pt-4">
         <h1 className="text-white">
-          N5 Brasil - Front-end Template
+          Front-end PWA Template
         </h1>
-        <WelcomeDaisyUI/>
-        <Copyright/>
+        <WelcomeDaisyUI />
+        <Copyright />
       </main>
     </div>
   );
