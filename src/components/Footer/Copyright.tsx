153eff4f65027a728c12361580085bf298cbde4b
Angelo Ricardo <angeloengcomp@gmail.com>
2023-03-24 16:19:38 -0300

    feat(copyright): add logotype

---
+++ src/components/Footer/Copyright.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export function Copyright() {
+  return (
+    <div className="py-4 border-t border-t-zinc-600 mt-10">
+      <div className="main_container">
+        <div className="flex items-center justify-center md:justify-between flex-col md:flex-row">
+          <p className="text-white text-sm m-0">
+            Freeooh {new Date().getFullYear()} ©
+            Todos os direitos reservados
+          </p>
+          <div className="flex items-center">
+            <p className="text-white flex items-center text-sm m-0">
+              <a href="https://www.bredi.com.br/" target="_blank">
+                Desenvolvido por Bredi{' '}
+                <img
+                  src="/bredi.png"
+                  alt="bredi tecnologia"
+                  width={18}
+                  className="inline-block"
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
