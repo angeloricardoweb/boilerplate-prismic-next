7eeac47cb322c240722e1fd9b89e30c4f5671b5c
Angelo Ricardo <angeloengcomp@gmail.com>
2023-03-26 15:21:43 -0300

    feat(axios): add default content-type and accept headers

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,4 +2,8 @@ import axios from 'axios'
 
 export const api = axios.create({
     baseURL: 'https://api.github.com/users/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    }
 })
