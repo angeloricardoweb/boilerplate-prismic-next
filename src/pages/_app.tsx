c9a1a5124078009c6661236b7332f3a5b290c457
Angelo Ricardo <angeloengcomp@gmail.com>
2023-03-27 17:24:48 -0300

    chore(swiper): add base config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Modals from '../components/Modals'
 import { BoxCookies } from '../components/Partials/BoxCookies'
 import { ToasterComponent } from '../components/Partials/ToasterComponent'
 import { GlobalContextProvider } from '../context/GlobalContextProvider'
+import 'swiper/css/bundle'
 import '../styles/index.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
