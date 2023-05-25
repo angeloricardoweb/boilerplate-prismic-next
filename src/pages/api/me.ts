594dd681aa8a9a050093aa9b82faf5a0f9b177f1
Angelo Ricardo <angeloengcomp@gmail.com>
2023-05-25 17:38:10 -0300

    feat: add api route example

--- src/pages/api/me.ts
+++ src/pages/api/me.ts
@@ -1,11 +1,14 @@
 import NextCors from 'nextjs-cors'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   await NextCors(req, res, {
-    // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     origin: '*',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200,
   })
 
   res.status(200).json({
