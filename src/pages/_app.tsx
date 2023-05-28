03d8f33c4caa720ffa073116fe03ca620a01ae0c
Angelo Ricardo <angeloengcomp@gmail.com>
2023-05-28 20:28:36 -0300

    feat: add loading when fetch server side data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,8 +13,32 @@ import { MenuHamburguer } from '../components/Partials/MenuHamburguer'
 import { DefaultSeo } from 'next-seo'
 import 'swiper/css/bundle'
 import '../styles/index.scss'
+import { Icon } from '@iconify/react'
+import { useEffect, useState } from 'react'
+import Router from 'next/router'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('finished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
+
   return (
     <>
       <DefaultSeo
@@ -46,7 +70,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
           {/* <PrismicPreview repositoryName={repositoryName}> */}
           <Header />
-          <Component {...pageProps} />
+          {loading ? (
+            <div className="h-screen flex items-center justify-center">
+              <Icon icon="mdi:loading" className="text-5xl animate-spin" />
+            </div>
+          ) : (
+            <Component {...pageProps} />
+          )}
           <Footer />
           <BoxCookies />
           <Modals />
