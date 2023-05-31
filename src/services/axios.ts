d249ac0ff15078d4d57df76fcc84e15ae8f6fadd
Angelo Ricardo <angeloengcomp@gmail.com>
2023-05-31 12:42:06 -0300

    fix: set localhost with default endpoint

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'https://api.github.com/users/',
+    baseURL: 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
