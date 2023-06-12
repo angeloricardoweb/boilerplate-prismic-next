af01e0be23aa2daeb136c3104d946f1e7309dca5
Angelo Ricardo <angeloengcomp@gmail.com>
2023-06-12 14:28:30 -0300

    feat: add get token in axios helper

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,9 +1,13 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const token = Cookies.get('token') || 'sem token nos cookies'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
+  baseURL: 'http://localhost:3000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
 })
