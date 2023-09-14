9dd58dfd50bbbf6dc93ceb6ed8ede9b601a649d3
Angelo Ricardo <angeloengcomp@gmail.com>
2023-09-14 12:06:57 -0300

    feat: improve menu mobile

--- src/components/Partials/MenuHamburguer.tsx
+++ src/components/Partials/MenuHamburguer.tsx
@@ -16,26 +16,41 @@ export function MenuHamburguer() {
 
   return (
     <div
-      className={`fixed z-50 w-[90%] md:hidden h-full bg-white top-0 flex items-center justify-center shadow-2xl ${
+      className={`fixed w-[90%] md:hidden h-full bg-[url(/img/background.png)] bg-cover bg-center bg-fixed top-0 flex items-center justify-center shadow-2xl z-[999999] bg-white/80 backdrop-blur-md ${
         showMenuHamburguer ? 'right-0' : '-right-[700px]'
       } transition-all`}
     >
       <div className="w-full h-full relative">
         <Icon
           icon="mdi:close"
-          className="text-black text-3xl absolute top-5 right-5 cursor-pointer"
+          className="text-3xl absolute top-5 right-5 cursor-pointer"
           onClick={() => setShowMenuHamburguer(false)}
         />
-        <div className="flex w-full h-full flex-col items-center justify-center">
+        <div className="py-20">
+          <div className="flex justify-center py-4 px-10">
+            <img src="/img/logo.png" alt="bredi" />
+          </div>
           {navLinks.map((link) => (
-            <span
+            <div
               key={link.name}
-              className="text-black text-2xl cursor-pointer"
+              className="text-2xl cursor-pointer text-zinc-900 bg-white p-5 border-b border-brand-yellow"
               onClick={() => handleRouting(link.route)}
             >
               {link.name}
-            </span>
+            </div>
           ))}
+          <div className="bg-white py-10">
+            <p className="text-zinc-900 text-center text-xl">
+              contato@site.com.br
+            </p>
+            <p className="text-zinc-900 text-center text-xl flex justify-center items-center">
+              <Icon
+                icon="mdi:whatsapp"
+                className="text-brand-yellow mr-2 text-xl"
+              />{' '}
+              (99) 9999-9999
+            </p>
+          </div>
         </div>
       </div>
     </div>
