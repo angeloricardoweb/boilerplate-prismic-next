6b05f863d2da4651d1ab20be8bc7f36df70bee6e
Angelo Ricardo <angeloengcomp@gmail.com>
2023-09-21 10:48:49 -0300

    ui: improve button

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 type Props = {
   children: React.ReactNode;
-  variant: 'primaryGreen' | 'primaryViolet' | 'primaryPink' | 'secondary' | 'outlinedWhite';
+  variant:
+    | 'primaryGreen'
+    | 'primaryViolet'
+    | 'primaryPink'
+    | 'secondary'
+    | 'outlinedWhite';
   color?: string;
   disabled?: boolean;
   onClick?: () => void;
   type?: 'button' | 'submit' | 'reset';
+  full?: boolean;
 };
 
 export function Button({
@@ -14,6 +20,8 @@ export function Button({
   variant = 'primaryGreen',
   disabled = false,
   onClick,
+  type = 'button',
+  full = false,
 }: Props) {
   const primaryGreen = 'bg-brand-green text-white';
   const primaryViolet = 'bg-brand-violet text-white';
@@ -22,14 +30,16 @@ export function Button({
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-full transition-all hover:opacity-75 px-4 py-2 mt-3
+      className={`mt-3 flex items-center justify-center gap-2 rounded-full px-4 py-2 transition-all hover:opacity-75
         ${variant === 'primaryGreen' && primaryGreen}
         ${variant === 'primaryViolet' && primaryViolet}
         ${variant === 'primaryPink' && primaryPink}
         ${variant === 'outlinedWhite' && outlinedWhite}
+        ${full ? 'w-full' : 'w-fit'}
       `}
       onClick={onClick}
       disabled={disabled}
+      type={type}
     >
       {children}
     </button>
