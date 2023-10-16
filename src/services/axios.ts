ed098d1e4d508424bcbf649974e1032710ebfd22
Angelo Ricardo <angeloengcomp@gmail.com>
2023-10-16 12:16:15 -0300

    feat: add interceptor when 401

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -25,3 +25,18 @@ api.interceptors.request.use(
     return Promise.reject(error)
   }
 )
+
+// intercepa 401 e manda para o login
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/login'
+    }
+
+    return Promise.reject(error)
+  }
+)
