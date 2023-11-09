fe0b4e24a06b03855ddc4b2b0ddaaf91431f8f1a
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-09 11:56:20 -0300

    Add responsive screen sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,28 @@ module.exports = {
         },
       },
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '3xl': '1920px',
+      // => @media (min-width: 1920px) { ... }
+
+      '4xl': '2560px',
+      // => @media (min-width: 2560px) { ... }
+    }
   },
   plugins: [require('@tailwindcss/line-clamp')],
 }
