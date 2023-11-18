94a49d7fb1c60a473863eca42c7557ee90668ba9
Angelo Ricardo <angeloengcomp@gmail.com>
2023-11-18 15:03:01 -0300

    Remove unnecessary Content-Type header

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,7 +4,6 @@ import Cookies from 'js-cookie'
 export const api = axios.create({
   baseURL: 'http://localhost:3000/api',
   headers: {
-    'Content-Type': 'application/json',
     Accept: 'application/json',
   },
 })
