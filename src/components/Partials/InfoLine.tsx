571bceb5e8143ef0174fa1e63e8a81f9a248aebc
Angelo Ricardo <angeloengcomp@gmail.com>
2024-01-06 16:07:12 -0300

    Refactor InfoLine component to use a div with separate strong and span elements

--- src/components/Partials/InfoLine.tsx
+++ src/components/Partials/InfoLine.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export function InfoLine({ title = "Title", value = "Value" }: { title: string, value: string }) {
     return (
-        <strong className='text-xl block'>{title}: {value}</strong>
+        <div className='text-xl'>
+            <strong >
+                {title}:{" "}
+            </strong>
+            <span>
+                {value}
+            </span>
+        </div>
     )
 }
