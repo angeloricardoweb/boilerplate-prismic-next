df3c0eff0a6e93c526e3adadb2c36a927a5f2b81
Angelo Ricardo <angeloengcomp@gmail.com>
2024-03-23 19:06:37 -0300

    Refactor privacy policy dialog and use dynamic cookie name

--- src/components/Dialogs/DialogPrivacityPolicy.tsx
+++ src/components/Dialogs/DialogPrivacityPolicy.tsx
@@ -1,30 +1,34 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { Button } from '../Buttons/Button'
+import sm from '../../../slicemachine.config.json'
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
+import { Button } from '../Buttons/Button';
+
 
 export default function DialogPrivacityPolicy() {
   const [open, setOpen] = useState(false)
 
+  const cookieName = `user-accept-cookies_${sm.repositoryName}`
+
   function setCookie() {
-    localStorage.setItem('user-accept-cookies', 'user-accept-cookies')
+    localStorage.setItem(cookieName, cookieName)
     setOpen(false)
   }
 
   useEffect(() => {
-    const cookie = localStorage.getItem('user-accept-cookies') || ''
-    if (!cookie.includes('user-accept-cookies')) {
+    const cookie = localStorage.getItem(cookieName) || ''
+    if (!cookie.includes(cookieName)) {
       setOpen(true)
     }
 
-  }, [])
+  }, [cookieName])
 
   return (
 
     <AlertDialog.Root open={open}
-      >
+    >
       <AlertDialog.Portal>
-        <AlertDialog.Content className="fixed bottom-2 border-t left-[50%] translate-x-[-50%] rounded-2xl bg-white p-5 w-full">
+        <AlertDialog.Content className="fixed bottom-2 border-t left-[50%] translate-x-[-50%] rounded-2xl bg-white p-5 w-full z-[9999]">
           <h2 className="text-2xl font-bold">Política de privacidade</h2>
           <p >
             Nós armazenamos dados temporariamente para melhorar a sua experiência de navegação e recomendar conteúdo de seu interesse. Ao utilizar nossos serviços, você concorda com tal monitoramento.
