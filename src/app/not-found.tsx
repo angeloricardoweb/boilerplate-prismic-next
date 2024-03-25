69b5c5cf89571d7a093e595864f334c5f9d8f8c3
Angelo Ricardo <angeloengcomp@gmail.com>
2024-03-25 14:18:16 -0300

    Fix bug in login functionality

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div className='flex items-center justify-center'>
+            <h1 className='text-3xl'>404 - Page Not Found</h1>
+        </div>
+    )
+}
