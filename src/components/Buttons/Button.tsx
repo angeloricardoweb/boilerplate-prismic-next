6b1ee1c3c50b0e2fcecd852ea38fbb3290c8b138
Angelo Ricardo <angeloengcomp@gmail.com>
2024-05-09 15:58:43 -0300

    refactor: Remove redundant CSS class in Button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -36,7 +36,7 @@ export function Button({
 
   return (
     <button
-      className={`mt-3 flex items-center justify-center gap-2 rounded-full px-4 py-2 transition-all hover:opacity-75 disabled:opacity-50
+      className={`flex items-center justify-center gap-2 rounded-full px-4 py-2 transition-all hover:opacity-75 disabled:opacity-50
         ${variant === 'primaryGreen' && primaryGreen}
         ${variant === 'primaryViolet' && primaryViolet}
         ${variant === 'primaryPink' && primaryPink}
