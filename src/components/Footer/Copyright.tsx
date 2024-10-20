332c79a39441d90923d3daec9e01d95308f1093c
Angelo Ricardo <angeloengcomp@gmail.com>
2024-10-20 18:08:29 -0300

    Refactor copyright text to use language data

--- src/components/Footer/Copyright.tsx
+++ src/components/Footer/Copyright.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Container } from '../Partials/Container'
+import useLang from '@/hooks/useLang'
+import { langData } from '@/location/langData'
 
 export function Copyright() {
+  const { stringData } = useLang()
+
   return (
     <div className="border-t border-t-zinc-600 py-4">
       <Container>
         <div className="flex flex-col items-center justify-center md:flex-row md:justify-between">
           <p className="m-0 text-xs text-center md:text-start">
-            Boilerplate Prismic Next 14 - {new Date().getFullYear()} © Todos os
-            direitos reservados
+            Boilerplate Prismic Next 14 - {new Date().getFullYear()} ©{' '}
+            {stringData(langData.AllRightsReserved)}
           </p>
 
           <div className="flex items-center">
--- src/location/langData.ts
+++ src/location/langData.ts
@@ -4,24 +4,24 @@ export const langData = {
     'pt-br': 'Home',
   },
   About: {
-    'en-us': 'About',
-    'pt-br': 'Sobre',
+    'en-us': 'About Us',
+    'pt-br': 'Quem Somos',
   },
-  Contact: {
-    'en-us': 'Contact',
-    'pt-br': 'Contato',
+  Products: {
+    'en-us': 'Products',
+    'pt-br': 'Produtos',
   },
-  Services: {
-    'en-us': 'Services',
-    'pt-br': 'Serviços',
-  },
-  Submenu: {
-    'en-us': 'Submenu',
-    'pt-br': 'Submenu',
+  Certifications: {
+    'en-us': 'Certifications',
+    'pt-br': 'Certificações',
   },
   Blog: {
     'en-us': 'Blog',
     'pt-br': 'Blog',
+  },
+  ContactUs: {
+    'en-us': 'Contact Us',
+    'pt-br': 'Fale conosco',
   },
   FrequentlyAskedQuestions: {
     'en-us': 'Frequently Asked Questions',
@@ -34,5 +34,21 @@ export const langData = {
   FollowUs: {
     'en-us': 'Follow Us',
     'pt-br': 'Siga-nos',
+  },
+  Testimonials: {
+    'en-us': 'Testimonials',
+    'pt-br': 'Depoimentos',
+  },
+  Atendment: {
+    'en-us': 'Atendment',
+    'pt-br': 'Atendimento',
+  },
+  WorkWithUs: {
+    'en-us': 'Work With Us',
+    'pt-br': 'Trabalhe Conosco',
+  },
+  AllRightsReserved: {
+    'en-us': 'All rights reserved',
+    'pt-br': 'Todos os direitos reservados',
   },
 }
