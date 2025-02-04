197ffe632e8bbf4b24c874074d0eb907dc678f5c
Mittzera <hugocavalcantesr@gmail.com>
2025-02-04 12:05:05 -0300

    feat: rework on button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -2,15 +2,24 @@
 import React from 'react'
 import { LoaderCircle } from 'lucide-react'
 
+// Tutorial de uso:
+// Caso precise adicionar um botão em seu projeto, você pode utilizar o componente Button. Para isso, basta importá-lo e utilizá-lo em seu código. Para personalizar o botão, você pode passar o nome da sua propriedade na área variant, que está com um comentário e depois adicionar uma nova prop na const VARIANT_CLASSES, que também estará comentada.
+// Na VARIANT_CLASSES você adiciona a estlização necessária para a sua página
+
 type Props = {
   children: React.ReactNode
-  variant: 'primaryGreen' | 'outlinedBlack'
-  color?: string
+  variant: 'primaryGreen' | 'outlinedBlack' // variant prop
   disabled?: boolean
-  onClick?: any
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
   type?: 'button' | 'submit' | 'reset'
   full?: boolean
   isLoading?: boolean
+}
+
+const VARIANT_CLASSES = {
+  // const VARIANT_CLASSES
+  primaryGreen: 'bg-brand-green-50 text-white',
+  outlinedBlack: 'bg-transparent border border-black text-black',
 }
 
 export function Button({
@@ -22,18 +31,23 @@ export function Button({
   full = false,
   isLoading = false,
 }: Props) {
-  const primaryGreen = 'bg-brand-green text-white'
-  const outlinedBlack = 'bg-transparent border border-black text-black'
+  const buttonClasses = `
+    flex items-center justify-center gap-2 rounded-md px-4 py-4 transition-all 
+    hover:opacity-75 disabled:opacity-50 
+    ${VARIANT_CLASSES[variant]} 
+    ${full ? 'w-full' : 'w-fit'}
+  `
 
   return (
     <button
-      className={`flex items-center justify-center gap-2 rounded-md px-4 py-2 transition-all hover:opacity-75 disabled:opacity-50 ${variant === 'primaryGreen' && primaryGreen} ${variant === 'outlinedBlack' && outlinedBlack} ${full ? 'w-full' : 'w-fit'} `}
+      className={buttonClasses}
       onClick={onClick}
       disabled={disabled}
       type={type}
       style={{
         whiteSpace: 'nowrap',
       }}
+      aria-busy={isLoading}
     >
       {isLoading ? (
         <LoaderCircle className="mx-4 size-5 animate-spin" />
